537d4d99793b3b4cac290f27020e80be062e0f36
Aakash <akashtembhare2002@gmail.com>
2025-02-05 15:18:04 +0530

    router

--- stepchange/src/Router/router.jsx
+++ stepchange/src/Router/router.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import App from "../App";
 import Index from "../components/Index";
 import Form1 from "../components/Form1";
@@ -6,33 +6,20 @@ import Form2 from "../components/Form2";
 import Form3 from "../components/Form3";
 import Thanks from "../components/Thanks";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Index />,
-      },
-      {
-        path: "/form1",
-        element: <Form1 />,
-      },
-      {
-        path: "/form2",
-        element: <Form2 />,
-      },
-      {
-        path: "/form3",
-        element: <Form3 />,
-      },
-      {
-        path: "/thankyoupage",
-        element: <Thanks />,
-      },
-    ],
-  },
-]);
+const RouterConfig = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Index />} />
+          <Route path="/form1" element={<Form1 />} />
+          <Route path="/form2" element={<Form2 />} />
+          <Route path="/form3" element={<Form3 />} />
+          <Route path="/thankyoupage" element={<Thanks />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
 
-export default router;
+export default RouterConfig;
--- stepchange/src/main.jsx
+++ stepchange/src/main.jsx
@@ -3,8 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
-import router from "./Router/router.jsx";
+import RouterConfig from "./Router/router.jsx"; // Import updated router
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router}/>
+  <React.StrictMode>
+    <RouterConfig /> {/* Now using HashRouter from router.js */}
+  </React.StrictMode>
 );
